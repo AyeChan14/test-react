bdc83c67fcaac0b65195c88e57a9602050503f71
AyeChan14 <meredaelinn263365765@gmail.com>
2025-03-09 11:35:51 +0630

    added file

---
+++ myproject/src/App.jsx
@@ -0,0 +1,9 @@
+function App()
+{
+  return(
+    <>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed commodi modi totam hic numquam itaque quia, ab cum maiores cupiditate, non nulla tempora, adipisci quos voluptate esse unde! Impedit, architecto?</p>
+    </>
+  )
+}
+export default App
